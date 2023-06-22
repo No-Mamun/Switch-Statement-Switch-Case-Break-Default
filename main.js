4d2d51057cc559a18bae01533a13d88742283fbb
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-06-22 15:56:35 +0600

    Add files via upload
    Switch Statement, Switch, Case, Break, Default

---
+++ main.js
@@ -0,0 +1,46 @@
+// Switch Statement, Switch, Case, Break,
+
+/*
+// No:1
+
+let person = "Teacher";
+
+switch (person){
+
+case "Teacher":console.log("Hello sir")
+
+break;
+
+case "Madam":console.log("Hi Mam")
+
+case "Robin":console.log("Hey Dosto!")
+
+default:console.log("we dont know you");
+
+
+}
+
+*/
+
+
+
+// No:2
+
+let person = "Madam";
+
+switch (person){
+
+case "Teacher":
+
+case "Madam":alert("Thang you")
+
+case "Principal":alert("Assalamu Alikum")
+
+break;
+
+case "Robin":console.log("Hey Dosto!")
+
+default:console.log("we dont know you");
+
+
+}
